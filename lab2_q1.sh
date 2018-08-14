0f989485d52fd438ab628098fc2f7201bba569f5
vidheeu <42140289+vidheeu@users.noreply.github.com>
2018-08-14 18:18:05 +0530

    Add files via upload

---
+++ lab2_q1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "what is your name"
+read name
+mkdir $name
+cd $name
+touch $name.cpp
+
---
+++ lab2_q2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "what is your name"
+read name
+mkdir $name
+cd $name
+for i in 1 2 3 4 5 
+do
+touch $name$i.cpp
+done
+
+
